a2c3c637abdf9637502984593c132e30921e4794
Gondogan <barbitasupc@gmail.com>
2026-05-21 10:19:53 +0200

    prueba de impresion

--- proyecto_Solitario/src/controller/Main.java
+++ proyecto_Solitario/src/controller/Main.java
@@ -8,6 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Carta carta1 = new Carta(13, Palo.TREBOLES, ColorCarta.NEGRO);
+		System.out.println("Funcionaa");
 		
 		try {
 			
